0f6ed13f30409a73523fadd561e115ae1a2f6ccd
Viper <drandran650@gmail.com>
2022-11-15 16:51:31 +0100

    PlayerMovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,18 +6,23 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public int numberOfJumps = 0;
+    public int maxNumberOfJumps = 1;
+    public float jumpForce;
 
     Vector2 movement = Vector2.zero;
 
     Rigidbody2D rb = null;
-    SpriteRenderer sr = null;
+    SpriteRenderer sprite = null;
     Animator animator = null;
+
+    private enum Movementstate { idle, run, jump, fall }
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
+        sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
 
     }
@@ -25,11 +30,70 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+        UpdateAnimationState();
 
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        ContactPoint2D contact = collision.GetContact(0);
+        if (contact.normal.y > 0.7f)
+        {
+            numberOfJumps = maxNumberOfJumps;
+        }
+    }
+    public void OnJump(InputValue val)
+    {
+        if (numberOfJumps <= 0)
+        {
+            return;
+        }
+        float innerValue = val.Get<float>();
+        if (innerValue > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            numberOfJumps--;
+        }
+    }
+
     public void OnMove(InputValue val)
     {
         movement = val.Get<Vector2>();
+    }
+
+    private void UpdateAnimationState()
+    {
+        Movementstate State;
+
+        if (movement.x > 0)
+        {
+            State = Movementstate.run;
+            sprite.flipX = false;
+        }
+
+        else if (movement.x < 0)
+        {
+            State = Movementstate.run;
+            sprite.flipX = true;
+        }
+
+        else
+        {
+            State = Movementstate.idle;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            State = Movementstate.jump;
+        }
+
+        else if (rb.velocity.y < -.1f)
+        {
+            State = Movementstate.fall;
+        }
+
+        animator.SetInteger("State", (int)State);
     }
 
 }
