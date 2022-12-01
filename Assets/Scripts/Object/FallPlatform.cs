45715936ef555e959c22e138e94ed0453b0ba4f0
Maxime HARRY <maxime.harry@e-artsup.net>
2022-12-01 16:28:02 +0100

    add movable and falling platform script

--- Assets/Scripts/Object/FallPlatform.cs
+++ Assets/Scripts/Object/FallPlatform.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parachute : MonoBehaviour
+public class FallPlatform : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,4 +18,10 @@ public class Parachute : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.AddForce(Physics2D.gravity * rb.mass);
+    }
+
 }
