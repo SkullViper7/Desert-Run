06069f00b257ae34b4df1c79802eb533d1dd4bff
Kaerousse <alexiadamay@gmail.com>
2022-12-08 18:25:08 +0100

    Addition of an options menu, changed fonts and colors of menu to fit other assets, a tweaking of the death counter

---
+++ Assets/UI/UIScripts/OptionsSetup.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class OptionsSetup : MonoBehaviour
+{
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    public Resolution[] resolutions;
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("ResolutionPref", resolutionDropdown.value);
+        PlayerPrefs.SetInt("FullscreenPref", Convert.ToInt32(Screen.fullScreen));
+
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("ResolutionPref"))
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPref");
+        }
+
+        else
+        {
+            resolutionDropdown.value = Convert.ToInt32(Screen.currentResolution);
+        }
+
+        if (PlayerPrefs.HasKey("FullsreenPref"))
+        {
+            Screen.fullScreen = Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPref"));
+        }
+
+        else
+        {
+            Screen.fullScreen = true;
+        }
+
+
+    }
+
+    private void Start()
+    {
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+                
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
+
+    }
+
+}
