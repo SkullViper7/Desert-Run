105cdf96e4b2ce22032ba47058955ea951a5fdd0
Viper <drandran650@gmail.com>
2022-12-13 15:18:54 +0100

    reversed spikes

---
+++ Assets/Scripts/Background/ReversedKillZone.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+public class ReversedKillZone : MonoBehaviour
+{
+    public Transform firstSpawn;
+    public Transform secondSpawn;
+
+    public CameraTrigger ct;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (!ct.isInSecondRoom)
+            {
+                other.gameObject.transform.position = firstSpawn.position;
+                other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                SingleTonReload.Instance.SceneResetAll();
+
+                PlayerPrefs.SetInt("deathCount", PlayerPrefs.GetInt("deathCount") + 1);
+            }
+            else
+            {
+                other.gameObject.transform.position = secondSpawn.position;
+                other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                SingleTonReload.Instance.SceneResetAll();
+
+                PlayerPrefs.SetInt("deathCount", PlayerPrefs.GetInt("deathCount") + 1);
+            }            
+        }
+    }
+
+}
